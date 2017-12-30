b3c3f36aba1077c281aad7b56265ab409ce185a6
barakd <barak.drechsler@gmail.com>
2017-12-30 15:27:45 +0200

    adding the porper License

--- withWorkers/utils.js
+++ withWorkers/utils.js
@@ -1,3 +1,6 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 var cache = { element: null, cx: null, id: null, height: 0, width: 0 };
 
 function canvasSetFromABGRBytes(canvasElt, bytes, height, width) {
--- worker.js
+++
@@ -1,4 +0,0 @@
-
-const textDecode = new textDecode();
-textDecode.decode(Uint8Array.from(arr)); // returns hello world
-
